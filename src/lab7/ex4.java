3e870bce457367e736fe203e8c66b3b175eb00a1
karn <natthakarnkarn29@gmail.com>
2019-10-06 15:11:51 +0700

    lab7

--- src/lab7/ex4.java
+++ src/lab7/ex4.java
@@ -4,7 +4,7 @@ public class ex4 {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i < 10; i++) {
             if (i %2 ==0) {
                 System.out.println("hello.");
                 System.out.println("\tKarnkarn");
